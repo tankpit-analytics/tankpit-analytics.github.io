4f90ed2ed5059c693e19a913e4f717590596adf6
Tom <tankpitanalytics@gmail.com>
2020-02-17 22:48:31 -0800

    add award search

--- js/awards-search.js
+++ js/awards-search.js
@@ -1,3 +1,78 @@
+function showStar1() {
+    $('#awards_search_star1').css({'display': 'table'});
+    $('#awards_search_header_star1').css({'display': 'block'});
+}
+
+function showStar2() {
+    $('#awards_search_star2').css({'display': 'table'});
+    $('#awards_search_header_star2').css({'display': 'block'});
+}
+
+function showStar3() {
+    $('#awards_search_star3').css({'display': 'table'});
+    $('#awards_search_header_star3').css({'display': 'block'});
+}
+
+function showTank1() {
+    $('#awards_search_tank1').css({'display': 'table'});
+    $('#awards_search_header_tank1').css({'display': 'block'});
+}
+
+function showTank2() {
+    $('#awards_search_tank2').css({'display': 'table'});
+    $('#awards_search_header_tank2').css({'display': 'block'});
+}
+
+function showTank3() {
+    $('#awards_search_tank3').css({'display': 'table'});
+    $('#awards_search_header_tank3').css({'display': 'block'});
+}
+
+function showMedal1() {
+    $('#awards_search_medal1').css({'display': 'table'});
+    $('#awards_search_header_medal1').css({'display': 'block'});
+}
+
+function showMedal2() {
+    $('#awards_search_medal2').css({'display': 'table'});
+    $('#awards_search_header_medal2').css({'display': 'block'});
+}
+
+function showMedal3() {
+    $('#awards_search_medal3').css({'display': 'table'});
+    $('#awards_search_header_medal3').css({'display': 'block'});
+}
+
+function showSword1() {
+    $('#awards_search_sword1').css({'display': 'table'});
+    $('#awards_search_header_sword1').css({'display': 'block'});
+}
+
+function showSword2() {
+    $('#awards_search_sword2').css({'display': 'table'});
+    $('#awards_search_header_sword2').css({'display': 'block'});
+}
+
+function showSword3() {
+    $('#awards_search_sword3').css({'display': 'table'});
+    $('#awards_search_header_sword3').css({'display': 'block'});
+}
+
+function showCup1() {
+    $('#awards_search_cup1').css({'display': 'table'});
+    $('#awards_search_header_cup1').css({'display': 'block'});
+}
+
+function showCup2() {
+    $('#awards_search_cup2').css({'display': 'table'});
+    $('#awards_search_header_cup2').css({'display': 'block'});
+}
+
+function showCup3() {
+    $('#awards_search_cup3').css({'display': 'table'});
+    $('#awards_search_header_cup3').css({'display': 'block'});
+}
+
 function showDoT() {
     $('#awards_search_dot3').css({'display': 'table'});
     $('#awards_search_header_dot3').css({'display': 'block'});
@@ -16,6 +91,81 @@ function showWC() {
 function showLB() {
     $('#awards_search_lb1').css({'display': 'table'});
     $('#awards_search_header_lb1').css({'display': 'block'});
+}
+
+function hideStar1() {
+    $('#awards_search_star1').css({'display': 'none'});
+    $('#awards_search_header_star1').css({'display': 'none'});
+}
+
+function hideStar2() {
+    $('#awards_search_star2').css({'display': 'none'});
+    $('#awards_search_header_star2').css({'display': 'none'});
+}
+
+function hideStar3() {
+    $('#awards_search_star3').css({'display': 'none'});
+    $('#awards_search_header_star3').css({'display': 'none'});
+}
+
+function hideTank1() {
+    $('#awards_search_tank1').css({'display': 'none'});
+    $('#awards_search_header_tank1').css({'display': 'none'});
+}
+
+function hideTank2() {
+    $('#awards_search_tank2').css({'display': 'none'});
+    $('#awards_search_header_tank2').css({'display': 'none'});
+}
+
+function hideTank3() {
+    $('#awards_search_tank3').css({'display': 'none'});
+    $('#awards_search_header_tank3').css({'display': 'none'});
+}
+
+function hideMedal1() {
+    $('#awards_search_medal1').css({'display': 'none'});
+    $('#awards_search_header_medal1').css({'display': 'none'});
+}
+
+function hideMedal2() {
+    $('#awards_search_medal2').css({'display': 'none'});
+    $('#awards_search_header_medal2').css({'display': 'none'});
+}
+
+function hideMedal3() {
+    $('#awards_search_medal3').css({'display': 'none'});
+    $('#awards_search_header_medal3').css({'display': 'none'});
+}
+
+function hideSword1() {
+    $('#awards_search_sword1').css({'display': 'none'});
+    $('#awards_search_header_sword1').css({'display': 'none'});
+}
+
+function hideSword2() {
+    $('#awards_search_sword2').css({'display': 'none'});
+    $('#awards_search_header_sword2').css({'display': 'none'});
+}
+
+function hideSword3() {
+    $('#awards_search_sword3').css({'display': 'none'});
+    $('#awards_search_header_sword3').css({'display': 'none'});
+}
+
+function hideCup1() {
+    $('#awards_search_cup1').css({'display': 'none'});
+    $('#awards_search_header_cup1').css({'display': 'none'});
+}
+
+function hideCup2() {
+    $('#awards_search_cup2').css({'display': 'none'});
+    $('#awards_search_header_cup2').css({'display': 'none'});
+}
+
+function hideCup3() {
+    $('#awards_search_cup3').css({'display': 'none'});
+    $('#awards_search_header_cup3').css({'display': 'none'});
 }
 
 function hideDoT() {
@@ -38,8 +188,352 @@ function hideLB() {
     $('#awards_search_header_lb1').css({'display': 'none'});
 }
 
+function searchStar1() {
+    showStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchStar2() {
+    hideStar1();
+    showStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchStar3() {
+    hideStar1();
+    hideStar2();
+    showStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchTank1() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    showTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchTank2() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    showTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchTank3() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    showTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchMedal1() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    showMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchMedal2() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    showMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchMedal3() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    showMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchSword1() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    showSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchSword2() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    showSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchSword3() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    showSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchCup1() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    showCup1();
+    hideCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchCup2() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    showCup2();
+    hideCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
+
+function searchCup3() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    showCup3();
+    hideDoT();
+    hidePH();
+    hideWC();
+    hideLB();
+}
 
 function searchDoT() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
     showDoT();
     hidePH();
     hideWC();
@@ -47,6 +541,21 @@ function searchDoT() {
 }
 
 function searchPH() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
     hideDoT();
     showPH();
     hideWC();
@@ -54,6 +563,21 @@ function searchPH() {
 }
 
 function searchLB() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
     hideDoT();
     hidePH();
     hideWC();
@@ -61,6 +585,21 @@ function searchLB() {
 }
 
 function searchWC() {
+    hideStar1();
+    hideStar2();
+    hideStar3();
+    hideTank1();
+    hideTank2();
+    hideTank3();
+    hideMedal1();
+    hideMedal2();
+    hideMedal3();
+    hideSword1();
+    hideSword2();
+    hideSword3();
+    hideCup1();
+    hideCup2();
+    hideCup3();
     hideDoT();
     hidePH();
     showWC();
