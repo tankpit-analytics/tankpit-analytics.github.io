13cd7f1a756daaf5c5c8e5ce8fead86dfcd60fa5
Tom <tankpitanalytics@gmail.com>
2020-02-19 17:10:41 -0800

    testing

--- scripts/update_daily.sh
+++ scripts/update_daily.sh
@@ -22,21 +22,21 @@ $PYTHON $SCRIPTSDIR/pop_t25_2020.py >> $STDLOG 2>&1
 $PYTHON $SCRIPTSDIR/pop_t100_overall_passes.py >> $STDLOG 2>&1
 $PYTHON $SCRIPTSDIR/pop_t25_2020_passes.py >> $STDLOG 2>&1
 
-# data
-$PYTHON $SCRIPTSDIR/get_2020_tanks.py >> $LOGDIR/get_2020_tanks.log 2>&1 # 14 secs
-$PYTHON $SCRIPTSDIR/get_all_tanks.py >> $LOGDIR/get_all_tanks.log 2>&1 # 14 mins
-$PYTHON $SCRIPTSDIR/get_2020_tanks_stats.py >> $LOGDIR/get_2020_tanks_stats.log 2>&1 # 4 mins
-$PYTHON $SCRIPTSDIR/get_all_tanks_stats.py >> $LOGDIR/get_all_tanks_stats.log 2>&1 # 3 hours 14 mins
+# # data
+# $PYTHON $SCRIPTSDIR/get_2020_tanks.py >> $LOGDIR/get_2020_tanks.log 2>&1 # 14 secs
+# $PYTHON $SCRIPTSDIR/get_all_tanks.py >> $LOGDIR/get_all_tanks.log 2>&1 # 14 mins
+# $PYTHON $SCRIPTSDIR/get_2020_tanks_stats.py >> $LOGDIR/get_2020_tanks_stats.log 2>&1 # 4 mins
+# $PYTHON $SCRIPTSDIR/get_all_tanks_stats.py >> $LOGDIR/get_all_tanks_stats.log 2>&1 # 3 hours 14 mins
 
-# populate the rest (last updated: all_tanks.csv / 2020_tanks.csv - above "data")
-$PYTHON $SCRIPTSDIR/pop_awards_search.py >> $STDLOG 2>&1
-$PYTHON $SCRIPTSDIR/pop_cups_leaderboard.py >> $STDLOG 2>&1
-$PYTHON $SCRIPTSDIR/pop_stats_overall.py >> $STDLOG 2>&1
-$PYTHON $SCRIPTSDIR/pop_stats_2020.py >> $STDLOG 2>&1
+# # populate the rest (last updated: all_tanks.csv / 2020_tanks.csv - above "data")
+# $PYTHON $SCRIPTSDIR/pop_awards_search.py >> $STDLOG 2>&1
+# $PYTHON $SCRIPTSDIR/pop_cups_leaderboard.py >> $STDLOG 2>&1
+# $PYTHON $SCRIPTSDIR/pop_stats_overall.py >> $STDLOG 2>&1
+# $PYTHON $SCRIPTSDIR/pop_stats_2020.py >> $STDLOG 2>&1
 
 # push
 git add .
-git commit -a -m "Automated commit triggered."
+git commit -a -m "Automated commit triggered. TESTING."
 git push origin master
 
 # log
