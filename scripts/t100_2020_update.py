ed7964e121b7a4e07211bc2e6bf44a6e3fddbefc
Tom <tankpitanalytics@gmail.com>
2020-02-24 11:02:23 -0800

    update 5m checks

--- scripts/t100_2020_update.py
+++ scripts/t100_2020_update.py
@@ -4,8 +4,7 @@ from t100_main import *
 
 if __name__ == '__main__':
 	start_5m_job_running()
-    master_df = update_all_ranks(
-        leaderboard = '2020',
+    master_df = update_all_ranks(leaderboard = '2020',
         master_csv_file = master_csv_2020,
         passes_csv_file = passes_csv_2020,
         ranks_dict = ranks_dict_2020,
--- scripts/t100_overall_update.py
+++ scripts/t100_overall_update.py
@@ -4,8 +4,7 @@ from t100_main import *
 
 if __name__ == '__main__':
 	start_5m_job_running()
-    master_df = update_all_ranks(
-        leaderboard = 'overall',
+    master_df = update_all_ranks(leaderboard = 'overall',
         master_csv_file = master_csv_overall,
         passes_csv_file = passes_csv_overall,
         ranks_dict = ranks_dict_overall,
