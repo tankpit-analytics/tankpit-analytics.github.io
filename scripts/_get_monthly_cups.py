9ebff7b0342aea65f6a0a3d7986f17f9d7805bcf
Tom <tankpitanalytics@gmail.com>
2020-05-22 13:53:01 -0700

    get monthly cups... stopping progress for now

--- scripts/_get_monthly_cups.py
+++ scripts/_get_monthly_cups.py
@@ -32,6 +32,8 @@ def get_tourn_results_dict(tourn_id):
     print('getting results for tourn_id:', str(tourn_id))
     return(get_dict_from_url('https://tankpit.com/api/tournament_results?tournament_id=' + str(tourn_id)))
 
+#----- part 1. get tourn top3
+
 # gets: (1) start time, (2) top 3 tank_id
 def get_tourn_top3_df(tourn_id, df_tourn_id_col = df_tourn_id_col):
     tourn_dict = get_tourn_results_dict(tourn_id)
@@ -48,8 +50,6 @@ def get_tourn_top3_df(tourn_id, df_tourn_id_col = df_tourn_id_col):
         })
     df = df[[df_tourn_id_col, 'start_time', 'first_place_tank_id', 'second_place_tank_id', 'third_place_tank_id']]
     return(df)
-
-#----- part 1. get tourn top3
 
 def update_tourn_top3(df, tourn_id):
     if check_is_done_tourn_id(tourn_id, df) == False:
@@ -71,45 +71,58 @@ def loop_update_tourn_top3(df, tourn_id_list, tourn_id_skip_list):
 #-- 2a. get signature tank_id
 
 # reworked version of get_cup_count()+run_full_loop() in get_all_tank_stats.py: takes a tank_id and gets the best cup count checking all associated tanks until found
-def get_best_cup_dict(tank_id, verbose = False):
+def get_best_cup_dict(tank_id, signature_cup_df, verbose = False):
     i_tank_dict, i_cup_dict, i_stats_dict = get_tank_cup_stats_dict(tank_id)
     # i+j = best cups [j stats within get_cup_count()]
     best_cup_dict = {'gold': 0, 'silver': 0, 'bronze': 0, 'total': 0}
     best_cups = 0
     i_cups = 0
     best_j_id = -1
-    i_or_j_str = ''
+    has_identified_signature = ''
     if i_cup_dict['total'] > 0:
         i_cups = i_cup_dict['total'] 
         best_cups = i_cups
         best_cup_dict = i_cup_dict
-        i_or_j_str = '- i ' + str(best_cup_dict)
         j_count = 0
     else:
         # traverse through other tanks
         try:
             j_count = 0
+            has_identified_signature_in_other_tanks_list = False
             for j_id in i_tank_dict['other_tanks']:
-                j_count += 1
-                j_tank_dict, j_cup_dict, j_stats_dict = get_tank_cup_stats_dict(j_id)
-                # compare cup counts and choose best
-                j_cups = 0
-                if j_cup_dict['total'] > 0:
-                    j_cups = j_cup_dict['total']
-                if j_cups > best_cups:
-                    best_cups = j_cups
-                    best_cup_dict = j_cup_dict
-                    i_or_j_str = 'j ' + str(best_cup_dict)
-                    best_j_id = j_id
+                if j_id in list(signature_cup_df['id']):
+                    best_cup_dict = {
+                        'gold': signature_cup_df[signature_cup_df['id'] == j_id, 'gold'],
+                        'silver': signature_cup_df[signature_cup_df['id'] == j_id, 'silver'],
+                        'bronze': signature_cup_df[signature_cup_df['id'] == j_id, 'bronze'],
+                        'total': signature_cup_df[signature_cup_df['id'] == j_id, 'total']
+                    }
+                    has_identified_signature_in_other_tanks_list = True
                     break
+            if has_identified_signature_in_other_tanks_list == True:
+                j_count += 1
+                has_identified_signature = '*****************************************'
+                pass
+            else:
+                for j_id in i_tank_dict['other_tanks']:
+                    j_count += 1
+                    j_tank_dict, j_cup_dict, j_stats_dict = get_tank_cup_stats_dict(j_id)
+                    # compare cup counts and choose best
+                    j_cups = 0
+                    if j_cup_dict['total'] > 0:
+                        j_cups = j_cup_dict['total']
+                    if j_cups > best_cups:
+                        best_cups = j_cups
+                        best_cup_dict = j_cup_dict
+                        break
         except:
             pass
     if verbose == True:
-        print('(tank_id: ' + str(tank_id) + ')', '- Number of other tanks:', j_count, i_or_j_str)
+        print('(tank_id: ' + str(tank_id) + ')', '- Number of other tanks checked:', j_count, has_identified_signature)
     return(best_cup_dict)
 
 def find_signature_tank_id(tank_id, signature_cup_df, verbose = False):
-    best_cup_dict = get_best_cup_dict(tank_id, verbose)
+    best_cup_dict = get_best_cup_dict(tank_id, signature_cup_df, verbose)
     try:
         signature_tank_id = int(signature_cup_df.loc[(signature_cup_df['gold'] == best_cup_dict['gold']) & 
                 (signature_cup_df['silver'] == best_cup_dict['silver']) &
@@ -152,7 +165,7 @@ if __name__ == '__main__':
     tourn_top3_with_signature = pd.read_csv(tourn_top3_with_signature_csv)
     #
     #for i in range(len(tourn_top3_df)):
-    for i in range(0,50):
+    for i in range(0,1000):
         print('#####', i)
         tmp_tourn_id = int(tourn_top3_df.loc[i, 'tourn_id'])
         if tmp_tourn_id not in list(tourn_top3_with_signature[df_tourn_id_col]):
